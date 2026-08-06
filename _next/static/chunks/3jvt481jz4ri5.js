7d4876d3fba3ab44305a97055f20bdcef1a64d4b
Alejandro Lozano <calivallelozano@gmail.com>
2026-08-06 15:56:58 -0500

    Publish static portfolio

---
+++ _next/static/chunks/3jvt481jz4ri5.js
@@ -0,0 +1,1 @@
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,80401,t=>{t.v(c=>Promise.all(["static/chunks/08px67ttwfcln.js"].map(c=>t.l(c))).then(()=>c(72462)))}]);
